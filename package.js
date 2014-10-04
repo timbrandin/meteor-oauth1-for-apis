53063e31283aa7d7a3cf47aaef12c70e823ced43
Tim Brandin <tim.brandin@gmail.com>
2014-10-04 19:31:44 +0200

    Added git path.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "Better OAuth 1 for API's - solves an issue with other (content, data) options being cut off.",
-  version: "1.0.0",
-  git: " \* Fill me in! *\ "
+  version: "1.0.1",
+  git: "https://github.com/timbrandin/meteor-oauth1-for-apis"
 });
 
 Package.onUse(function(api) {
